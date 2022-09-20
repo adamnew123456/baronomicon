aefdfc11d79d6296264b09d21a60cbfc56a5c263
adamnew123456 <adamnew123456@gmail.com>
2022-09-19 20:13:57 -0400

    Support for item tags
    Item definitions can include any number of tags in addition to its
    identifier, and recipes can reference an item by its ID or a group of
    items by a tag. The EK Armory mod uses these in some places to manage
    suits that use different designs but function the same way for crafting.
    
    Items without any fabrication or deconstruction recipes are filtered
    out. This will be improved later to look for dependencies and only
    discard items that have no references at all.

--- extractor/bin/main.ml
+++ extractor/bin/main.ml
@@ -70,22 +70,26 @@ let ignore_subtree (stream: Xmlm.input) (k: Xmlm.input -> 'a) =
   ignore 1
 
 (** Fetches a value from an association list, used for XMl attributes. *)
-let rec assoc (alist: ('a * 'b) list) (key: 'a) =
+let rec find_attr (alist: ((string * string) * 'a) list) (attr: string) =
   match alist with
-  | (akey, aval) :: rest ->
-    if akey = key then
+  | ((_, akey), aval) :: rest ->
+    if String.lowercase_ascii akey = String.lowercase_ascii attr then
       Some aval
     else
-      assoc rest key
+      find_attr rest attr
 
   | [] -> None
 
 (* Data model *)
 
+type item_reference =
+  | ItemId of string
+  | ItemTag of string
+
 (** An item used to fabricate another item, or that comes from deconstructing
     that item. *)
 type recipe_part = {
-  item_id: string; (* The ID of the item consumed in the recipe. *)
+  item: item_reference;
   amount: int;
   min_condition: float option;
   max_condition: float option;
@@ -97,6 +101,7 @@ type recipe = recipe_part list
 (** An individual item, including the recipes for fabricating or destructing it (if supported)*)
 type item_decl = {
   id: string;
+  tags: string list;
   fabricate: recipe list;
   deconstruct: recipe list;
 }
@@ -104,7 +109,11 @@ type item_decl = {
 (* Serialization *)
 
 let recipe_part_to_json (part: recipe_part) : Yojson.t =
-  `Assoc [("id", `String part.item_id);
+  let item_ref =
+    match part.item with
+    | ItemId id -> `Assoc [("id", `String id)]
+    | ItemTag tag -> `Assoc [("tag", `String tag)] in
+  `Assoc [("item", item_ref);
           ("amount", `Int part.amount);
           ("min_condition",
            match part.min_condition with
@@ -120,6 +129,7 @@ let recipe_to_json (recipe: recipe) : Yojson.t =
 
 let item_decl_to_json (item: item_decl) : Yojson.t =
   `Assoc [("id", `String item.id);
+          ("tags", `List (List.map (fun tag -> `String tag) item.tags));
           ("fabricate", `List (List.map recipe_to_json item.fabricate));
           ("deconstruct", `List (List.map recipe_to_json item.deconstruct))]
 
@@ -128,18 +138,22 @@ let item_decl_to_json (item: item_decl) : Yojson.t =
 (** Parses an Item file from XML into a list of item declarations. *)
 let parse_item_file (path: string) =
   let rec build_recipe_part attrs =
-    let item_id = assoc attrs ("", "identifier") |> Option.get in
+    let item =
+      match (find_attr attrs "identifier", find_attr attrs "tag") with
+      | (Some id, _) -> ItemId id
+      | (_, Some tag) -> ItemTag tag
+      | _ -> raise Not_found in
     let amount =
-      match assoc attrs ("", "amount") with
+      match find_attr attrs "amount" with
       | Some amount_str -> int_of_string amount_str
       | None -> 1 in
-    let use_condition = assoc attrs ("", "usecondition") = Some "false" in
+    let use_condition = find_attr attrs "usecondition" = Some "false" in
     if use_condition then
-      {item_id; amount; min_condition=None; max_condition=None}
+      {item; amount; min_condition=None; max_condition=None}
     else
-      let min_condition = assoc attrs ("", "minconditon") |> Option.map float_of_string in
-      let max_condition = assoc attrs ("", "maxcondition") |> Option.map float_of_string in
-      {item_id; amount; min_condition; max_condition}
+      let min_condition = find_attr attrs "mincondition" |> Option.map float_of_string in
+      let max_condition = find_attr attrs "maxcondition" |> Option.map float_of_string in
+      {item; amount; min_condition; max_condition}
 
   and parse_fabricate items item_builder fab_list stream =
     match Xmlm.input stream with
@@ -185,10 +199,16 @@ let parse_item_file (path: string) =
     match Xmlm.input stream with
     | `El_start ((_, _), attrs) ->
       begin
-        match assoc attrs ("", "identifier") with
+        match find_attr attrs "identifier" with
         | Some item_id ->
+          let tags =
+            find_attr attrs "tags"
+            |> Option.map (String.split_on_char ',')
+            |> Option.map (List.map String.trim)
+            |> Option.value ~default:[] in
           let item_builder = {
             id=item_id;
+            tags;
             fabricate=[];
             deconstruct=[]} in
           parse_item items item_builder stream
@@ -218,6 +238,7 @@ let () =
       |> List.tl
       |> List.concat_map (readdir_recursive ".xml")
       |> List.concat_map parse_item_file
+      |> List.filter (fun item -> List.length item.deconstruct > 0 || List.length item.fabricate > 0)
       |> List.map item_decl_to_json in
     begin
       Yojson.to_channel Out_channel.stdout (`List items) ;
